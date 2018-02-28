915d57c872c6aeed4cc59c7eb69ea062dfad9459
Raghuttam Hombal <group4pgs@gmail.com>
2018-02-28 20:05:59 +0530

    py_wall.py
    This is for setting NASA_APOD picture of the Day as your desktop background

--- py_wall.py
+++ py_wall.py
@@ -3,11 +3,11 @@ import json
 import urllib
 import os
 
-os.system('export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules/')
+os.system('export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules/')	#to remove backend error
 url = urllib.urlopen('https://api.nasa.gov/planetary/apod?api_key=LBzdGVRkhVayk4DL6sxLtUuBv0gc1cy0goHcvLNz')
 urlj = json.load(url)
 
-if urlj['media_type'] == 'image':
+if urlj['media_type'] == 'image':	#to check if the element is picture only
 	urllib.urlretrieve(urlj['hdurl'],'/home/raghuttam/apod_img.jpg')
 else:
 	print("The image is unavailable today as the server has uploaded a "+urlj['media_type'])
